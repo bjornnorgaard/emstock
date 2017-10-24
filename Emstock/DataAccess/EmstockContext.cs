6af3d144f426ee6859c246a961da452b07237b1e
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-10-24 14:03:39 +0200

    Basic setup done

---
+++ Emstock/DataAccess/EmstockContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class EmstockContext : DbContext
+    {
+        public EmstockContext(DbContextOptions<EmstockContext> options) : base(options) { }
+
+        public EmstockContext() { }
+
+        public DbSet<Models.Component> Components { get; set; }
+    }
+}
---
+++ Emstock/Models/Component.cs
@@ -0,0 +1,8 @@
+﻿namespace Models
+{
+    public class Component
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+    }
+}
